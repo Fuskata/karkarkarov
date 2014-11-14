9d9d7aa9d257bc6701c1cfa532b51845afad3319
unknown <elp1@k103.lyceum.local>
2014-11-14 16:36:15 +0300

    Creating blog

--- main/models.py
+++ main/models.py
@@ -1,4 +1,11 @@
 from django.db import models
-
-
 # Create your models here.
+class Post(models.Model):
+	header = models.CharField(max_length=255)
+	content = models.TextField()
+	when = models.DateTimeField()
+
+ 	
+	def __unicode__(self):
+		return u'{} at {}'.format(self.header,
+			self.when)
--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
-# from main.models import Post
+from main.models import Post
+from datetime import datetime
 # Create your views here.
 
 
 class Posts(View):
     def get(self, request):
-        return HttpResponse(render(request, 'posts.html', {}))
+    	november_start = datetime(2014, 11, 1)
+        posts = Post.objects.all().order_by('-when').filter(when__gt=november_start)
+        """ SELECT * from POST WHERE when>='1.11.2014 ORDER by when; """
+        return HttpResponse(render(request, 'posts.html', {'posts': posts}))
+
